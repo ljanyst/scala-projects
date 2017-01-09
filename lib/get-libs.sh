652b46ffc4306a43e081a188f9c66ba415b535de
Lukasz Janyst <lukasz@jany.st>
2017-01-09 20:55:13 +0100

    Add build system

---
+++ lib/get-libs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.11/3.0.1/scalatest_2.11-3.0.1.jar
+wget https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.11/3.0.1/scalactic_2.11-3.0.1.jar
